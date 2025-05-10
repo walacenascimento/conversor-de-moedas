4096001a43e8f806483e8ee22414a189b5812428
Walace Nascimento <wlacens@hotmail.com>
2025-05-09 22:51:56 -0300

    Implementando o App, chamando a classe Menu no App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+
+import Modelos.Menu;
+
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("****** Conversor de Moedas ******");
+
+        Menu menu = new Menu();
+        menu.menu();
+
     }
 }
